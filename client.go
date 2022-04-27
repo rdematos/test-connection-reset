1a09a421d40fd0cd73ccef68a5a36354c758aa64
Ricardo DeMatos <ricardodematos@rivian.com>
2022-04-27 11:26:24 -0700

    ur to enviroment var

--- client.go
+++ client.go
@@ -1,39 +1,37 @@
-package main
- 
+ackage main
+
 import (
+    "os"
     "log"
     "net/http"
-		"time"
-		"io/ioutil"
+    "time"
+    "io/ioutil"
 )
 
 func fetch(client *http.Client, url string) {
-	resp, err := client.Get(url)
-	if err != nil {
-			log.Printf("Failed to get url %v", err)
-			return
-	}
+        resp, err := client.Get(url)
+        if err != nil {
+                        log.Printf("Failed to get url %v", err)
+                        return
+        }
 
-	log.Printf("Fetched page, response code: %v", resp.StatusCode)
-	defer resp.Body.Close()
-	ioutil.ReadAll(resp.Body)
+        log.Printf("Fetched page, response code: %v", resp.StatusCode)
+        defer resp.Body.Close()
+        ioutil.ReadAll(resp.Body)
 }
 
 func main() {
-    url := "http://localhost:8000/"
-    waitPeriod := 500000 * time.Millisecond
- 
-		tr := &http.Transport{
-			DisableKeepAlives:   false,
-		}
-		client := &http.Client{Transport: tr}
+    url := os.Getenv("URL")
+    waitPeriod := 375000 * time.Millisecond
+
+                tr := &http.Transport{
+                        DisableKeepAlives:   false,
+                }
+                client := &http.Client{Transport: tr}
 
     for times := 1; times < 20000; times++ {
-        log.Printf("Fetch page, wait period: %d", waitPeriod)
- 
-				fetch(client, url)
-				
+        log.Printf("Fetch page " + url + " wait period: " + waitPeriod.String())
+        fetch(client, url)
         time.Sleep(waitPeriod)
-        waitPeriod -= 100 * time.Microsecond
     }
 }
